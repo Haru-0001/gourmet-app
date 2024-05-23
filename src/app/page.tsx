aa66e1020a33f36620742aeab01790cedbae63c5
haru <23bus1maesub@gmail.com>
2024-05-23 14:59:36 +0900

    cookie改良

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
   const skyState = sepHour(hour); //時間帯を取得
   const skyTheme = sepSkyTheme(skyState); //時間帯のテーマを取得
 
+
+
   return (
     <div className={`h-screen flex flex-col ${skyTheme}`}>
       <div className="my-8 md:my-12"><SendMessage skyState={skyState} /></div>
