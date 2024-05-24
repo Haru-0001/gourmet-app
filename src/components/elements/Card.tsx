686401c7768914ad16187eb97b0e505859ccc4d4
haru <23bus1maesub@gmail.com>
2024-05-24 14:39:10 +0900

    card の レイアウト変更

--- src/components/elements/Card.tsx
+++ src/components/elements/Card.tsx
@@ -8,12 +8,12 @@ const Card: NextPage<RestaurantCard> = ({ photo , title , pr , access}) => {
     const useAccess= access;
 
     return (
-        <div className="w-9/12 my-8 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-xl hover:bg-gray-100 md:flex-row md:max-w-2xl md:h-60">
+        <div className="w-9/12 my-4 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-xl hover:bg-gray-100 md:flex-row md:max-w-2xl md:h-60">
             <img className="object-cover w-full rounded-t-lg h-56 md:h-full md:w-48 md:rounded-none md:rounded-s-lg md:aspect-square" src={usePhoto}/>
             <div className="flex flex-col justify-between h-full p-4 space-y-2">
-                <h5 className="basis-1/3 text-2xl font-bold tracking-tight text-gray-900">{useTitle}</h5>
-                <p className="basis-1/3 font-normal text-gray-700">{usePr}</p>
-                <p className="basis-1/3 font-normal text-sm text-gray-500">{useAccess}</p>
+                <h3 className="basis-1/2 text-2xl font-bold tracking-tight text-gray-900 overflow-hidden">{useTitle}</h3>
+                <p className="basis-1/4 font-normal text-gray-700 line-clamp-2">{usePr}</p>
+                <p className="basis-1/4 font-normal text-sm text-gray-500 line-clamp-2 md:pt-1">{useAccess}</p>
             </div>
         </div>
     );
