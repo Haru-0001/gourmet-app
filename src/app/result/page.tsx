08457fd6057f81beffff3f6840edcadc55aa2c0e
haru <23bus1maesub@gmail.com>
2024-05-27 00:12:04 +0900

    Cardコンポーネントの見直し

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,43 +1,8 @@
 'use client'
-import { Card }from "@/components/elements/Card";
-import { GetLocalRestaurant } from "@/types/GetLocalRestaurant";
-import {useEffect, useState } from "react";
-import { getLocation } from "@/api/location/getLocation";
-import { getRestaurants } from "@/api/restaurants/default/getRestaurants";
-import { RestaurantCard } from "@/types/RestaurantCard";
-import { cardMapper } from "@/features/restaurant/card/cardMapper";
-import jsCookie from "js-cookie";
+import { Card } from "@/components/elements/Card";
 
-// ユーザーの位置情報を取得し、その位置情報を元にレストランのデータを取得する
 export default function result() {
-    const [cards, setCards] = useState<RestaurantCard[]>([]);
-
-    //useEffectを使用してgetUserRestaurantsを実行
-    useEffect(() => {
-        const getUserRestaurants = async () => {
-                const location = await getLocation()
-                const range = Number(jsCookie.get("range"))
-                const params: GetLocalRestaurant = {
-                    start: 1,
-                    range: range,
-                    latitude: location.latitude,
-                    longitude: location.longitude
-                }
-                const response = await getRestaurants(params)
-                const restaurantCards = cardMapper(response)
-                setCards(restaurantCards)
-            }
-        getUserRestaurants()
-    }, [])
-
     return (
-        // レストランのデータをカードに表示
-        <div>
-            <div className="flex flex-col items-center">
-            {cards?.map((restaurant: RestaurantCard) => (
-                <Card key={restaurant.key} photo={restaurant.photo} title={restaurant.title} pr={restaurant.pr} access={restaurant.access}/>
-            ))}
-            </div>
-        </div>
+        <div><Card /></div>
     )
 }
