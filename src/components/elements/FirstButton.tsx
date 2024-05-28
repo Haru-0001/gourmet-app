98dc144e5ddd393a4047a1ed8ee4ceb6c986763e
haru <23bus1maesub@gmail.com>
2024-05-28 21:43:28 +0900

    UIをテーマによって影響が出ないように微修正

--- src/components/elements/FirstButton.tsx
+++ src/components/elements/FirstButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const FirstButton= () => {
     return(
         <Link href={{pathname: '/result'}}>
-            <button className="flex justify-center items-center rounded-full size-80 border-2 border-slate-500/50 shadow-btn">
+            <button className="flex justify-center items-center rounded-full size-80 border-4 border-slate-800/30 shadow-btn">
             <svg className="size-9/12 " width="80" height="80" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
                 <path d="M 100 500 Q 100 375 200 350 Q 200 200 350 200 Q 500 50 650 200 Q 800 200 800 350 Q 900 375 900 500 L 950 500 Q 950 850 650 900 L 650 950 350 950 350 900 Q 50 850 50 500 Z" stroke="#fff" strokeWidth="75" fill="none" />
             </svg>
--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -32,7 +32,7 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-800">
       <SliderPrimitive.Range className="absolute h-full bg-slate-50" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-slate-50 bg-pink-400 ring-offset-pink-500 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-100 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-slate-50 bg-blue-900 ring-offset-blue-700 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-100 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
   )
 })
