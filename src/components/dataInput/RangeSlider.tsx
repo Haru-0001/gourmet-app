e0009c43f975c0951f6c39666ebdf0000b0829e5
haru <23bus1maesub@gmail.com>
2024-05-29 09:25:17 +0900

    不要なTailwindCSSを削除

--- src/components/dataInput/RangeSlider.tsx
+++ src/components/dataInput/RangeSlider.tsx
@@ -13,9 +13,9 @@ const RangeSlider = () => {
         <div className="relative mb-6 w-9/12">
             <Slider defaultValue={[2]} min={1} max={5} step={1} onValueChange={onValueChange}/>
             <span className="text-sm text-gray-500 absolute start-0 -bottom-6">300m</span>
-            <span className="text-sm text-gray-500 absolute start-1/4 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">500m</span>
-            <span className="text-sm text-gray-500 absolute start-1/2 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">1000m</span>
-            <span className="text-sm text-gray-500 absolute start-3/4 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2000m</span>
+            <span className="text-sm text-gray-500 absolute start-1/4 -translate-x-1/2 -bottom-6">500m</span>
+            <span className="text-sm text-gray-500 absolute start-1/2 -translate-x-1/2 -bottom-6">1000m</span>
+            <span className="text-sm text-gray-500 absolute start-3/4 -translate-x-1/2 -bottom-6">2000m</span>
             <span className="text-sm text-gray-500 absolute end-0 -bottom-6">3000m</span>
         </div>
     );
