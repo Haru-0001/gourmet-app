ccc69c3ee6f508619004613b30ad01dc57da4405
haru <23bus1maesub@gmail.com>
2024-05-31 01:09:33 +0900

    ディレクトリ構成の再見直し

--- src/components/modules/RangeSlider.tsx
+++ src/components/modules/RangeSlider.tsx
@@ -12,11 +12,11 @@ const RangeSlider = () => {
     return (
         <div className="relative mb-6 w-9/12">
             <Slider defaultValue={[2]} min={1} max={5} step={1} onValueChange={onValueChange}/>
-            <span className="text-sm text-gray-500 absolute start-0 -bottom-6">300m</span>
-            <span className="text-sm text-gray-500 absolute start-1/4 -translate-x-1/2 -bottom-6">500m</span>
+            <span className="text-sm text-gray-500 absolute start-[-12px] -bottom-6">&nbsp;300m</span>
+            <span className="text-sm text-gray-500 absolute start-1/4 -translate-x-1/2 -bottom-6">&emsp;500m</span>
             <span className="text-sm text-gray-500 absolute start-1/2 -translate-x-1/2 -bottom-6">1000m</span>
             <span className="text-sm text-gray-500 absolute start-3/4 -translate-x-1/2 -bottom-6">2000m</span>
-            <span className="text-sm text-gray-500 absolute end-0 -bottom-6">3000m</span>
+            <span className="text-sm text-gray-500 absolute end-[-12px] -bottom-6">3000m</span>
         </div>
     );
 }
