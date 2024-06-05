abdb53c9dfa4473d3ea92ea52c0ac0aeaa0e584b
haru <23bus1maesub@gmail.com>
2024-06-05 18:30:08 +0900

    時刻の取得方法とストレージの変更

--- src/components/elements/message/SendMessage.tsx
+++ src/components/elements/message/SendMessage.tsx
@@ -1,25 +1,12 @@
+import { messageTime } from "@/features/time/sepHour";
+import { themeMessage } from "@/store/themeData";
+import { MessageTimeType } from "@/types/ThemeTime";
+
 //時間帯によってメッセージを変えるコンポーネント
+const SendMessage = () => {
+    const time = messageTime() as MessageTimeType;
+    const message = themeMessage[time].firstMessage;
 
-const defaultMessage = "ワンタップで<br />お店を探そう！";
-const NightMessage = "今日も一日<br />お疲れ様です！";
-const MorningMessage = "おはようございます！<br />素敵な一日に<br />なりますように";
-
-interface SendMessageProps {
-    skyState: string;
-}
-
-function getSkyMessage(skyState: string): string {
-    if (skyState === "night") {
-        return NightMessage;
-    } else if (skyState === "morning") {
-        return MorningMessage;
-    } else {
-        return defaultMessage;
-    }
-}
-
-const SendMessage: React.FC<SendMessageProps> = ({ skyState }) => {
-    const message = getSkyMessage(skyState);
     return (
         <p className="text-4xl text-center md:text-6xl text-neutral-50"
             dangerouslySetInnerHTML={{ __html: message }}>
--- src/features/theme/skyTheme.ts
+++
@@ -1,7 +0,0 @@
-//各テーマの色を定義
-
-const nightTheme = "bg-indigo-950"
-const twilightTheme = "bg-gradient-to-b from-fuchsia-400 to-orange-400"
-const skyTheme = "bg-sky-300"
-
-export { nightTheme, twilightTheme, skyTheme };
