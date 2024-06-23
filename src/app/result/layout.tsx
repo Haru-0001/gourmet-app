fb24848a50e254ec3949272e80821ab0af5d9c5f
Haru-0001 <23bus1maesub@gmail.com>
2024-06-24 05:09:27 +0900

    Paginationのテンプレートを作成

--- src/app/result/layout.tsx
+++ src/app/result/layout.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/layouts/header/Header";
 import { Footer } from "@/components/layouts/footer/Footer";
+import { ResultPagination } from "@/components/modules/ResultPagination";
 
 export default function resultLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <div className="flex flex-col h-screen">
             <Header />
             {children}
+            <ResultPagination />
             <Footer />
         </div>
     );
--- src/components/modules/ResultPagination.tsx
+++ src/components/modules/ResultPagination.tsx
@@ -19,7 +19,16 @@ const ResultPagination = () => {
                     <PaginationLink href="#">1</PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
+                    <PaginationLink href="#">2</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
                     <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">10</PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
                     <PaginationNext href="#" />
