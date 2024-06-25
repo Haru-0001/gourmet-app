12a41fcc12ebb3d5b4da7d72cf388358a1bb72c0
Haru <23bus1maesub@gmail.com>
2024-06-26 05:38:24 +0900

    スライダーを無効化させるように修正

--- src/components/modules/RangeSlider.tsx
+++ src/components/modules/RangeSlider.tsx
@@ -18,10 +18,10 @@ const RangeSlider = (props:{time:ThemeTimeType}) => {
     const thumbTheme = themeColor[time].primaryAction;
     const ringTheme = themeColor[time].ring;
     const [isChecked] = useAtom(isSliderAtom);
-    const opacity = isChecked ? "opacity-100" : "opacity-50"; //スライダーの透明度をisSliderButtonで切り替えるための設定
+    const opacity = isChecked ? "opacity-100" : "opacity-50"; //スライダーのON/OFFをisSliderButtonで切り替えるための設定
     return (
         <div className={`relative mb-6 w-9/12 ${opacity}`}>
-            <Slider defaultValue={[2]} min={1} max={5} step={1} onValueChange={onValueChange} theme={{ thumbTheme, ringTheme }} />
+            <Slider defaultValue={[2]} min={1} max={5} step={1} disabled={!isChecked} onValueChange={onValueChange} theme={{ thumbTheme, ringTheme }} />
             <span className={`text-sm ${ textTheme } absolute start-[-12px] -bottom-6`}>&nbsp;300m</span>
             <span className={`text-sm ${ textTheme } absolute start-1/4 -translate-x-1/2 -bottom-6`}>&emsp;500m</span>
             <span className={`text-sm ${ textTheme } absolute start-1/2 -translate-x-1/2 -bottom-6`}>1000m</span>
