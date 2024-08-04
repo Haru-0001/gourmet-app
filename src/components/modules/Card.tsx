b2cda9defbbf44d7a06a39502768a3edbac29b66
Haru-0001 <23bus1maesub@gmail.com>
2024-08-04 23:19:56 +0900

    feat: cardの見出しを追加

--- src/components/modules/Card.tsx
+++ src/components/modules/Card.tsx
@@ -7,7 +7,7 @@ import { RestaurantCard } from "@/types/RestaurantCard";
 import { cardMapper } from "@/features/restaurant/card/cardMapper";
 import { useAtomValue, useSetAtom } from "jotai";
 import { cardLengthAtom, firstCardValueAtom, maxCardAtom } from "@/store/paginationAtom";
-import { rangeAtom } from "@/store/searchAtom";
+import { rangeAtom, rangeNumAtom } from "@/store/searchAtom";
 
 // ユーザーの位置情報を取得し、その位置情報を元にレストランのデータを取得する
 export const Card = () =>{
@@ -17,6 +17,7 @@ export const Card = () =>{
     const range = useAtomValue(rangeAtom);
     const setMaxPage = useSetAtom(maxCardAtom);
     const setCardLength = useSetAtom(cardLengthAtom);
+    const rangeNum = useAtomValue(rangeNumAtom);
 
     const getUserRestaurants = async () => {
         const location = await getLocation();
@@ -40,8 +41,12 @@ export const Card = () =>{
     return (
     // レストランのデータをカードに表示
     <div className="flex flex-col items-center my-12">
-    {cards ? (
-        cards.map((restaurant: RestaurantCard) => (
+    {cards.length >0 ? (
+        <>
+        {rangeNum !== undefined && (
+            <h2 className="p-4 text-xl">現在地から{rangeNum}m以内のレストランを表示中</h2>
+        )}
+        {cards.map((restaurant: RestaurantCard) => (
             <div key={restaurant.key} className="w-9/12 my-4 flex flex-col bg-white border border-gray-200 rounded-lg shadow-xl hover:bg-gray-100 md:flex-row md:max-w-2xl md:h-60">
             <img className="object-cover w-full rounded-t-lg h-56 md:h-full md:w-48 md:rounded-none md:rounded-s-lg md:aspect-square" src={restaurant.photo}/>
                 <div className="flex flex-col justify-between h-full p-4 space-y-2">
@@ -50,7 +55,8 @@ export const Card = () =>{
                     <p className="basis-1/4 font-normal text-sm text-gray-500 md:line-clamp-2 md:pt-1">{restaurant.access}</p>
                 </div>
             </div>
-        ))
+        ))}
+        </>
     ) : (
         <div className="text-gray-500">近くにレストランがありません</div>
     )}
--- src/store/searchAtom.ts
+++ src/store/searchAtom.ts
@@ -4,3 +4,22 @@ export const rangeAtom = atom(2)
 export const isSliderAtom = atom(true)
 export const searchBoxAtom = atom('')
 
+export const rangeNumAtom = atom((get) => {
+    const range = get(rangeAtom);
+    if(range === 1){
+        return 300;
+    }else if(range === 2){
+        return 500;
+    }
+    else if(range === 3){
+        return 1000;
+    }
+    else if(range === 4){
+        return 2000;
+    }
+    else if (range === 5){
+        return 3000;
+    }else{
+        return undefined;
+    }
+});
